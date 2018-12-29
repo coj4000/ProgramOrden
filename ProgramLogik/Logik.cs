a4b57eb93d312269499cef6cc2781cf404b67ea5
Christian Oftelund Jensen <chri9j83@edu.easj.dk>
2018-12-29 12:41:13 +0100

    Har addet mit program til github, og er begyndt at lave auto

---
+++ ProgramLogik/Logik.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using RestSharp;
+using System.Net;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramLogik
+{
+    public class Logik
+    {
+
+        public List<string> logicList = new List<string>();
+        public List<string> listeStrings;
+        public Logik()
+        {
+
+        }
+        public Logik(string path)
+        {
+            DrevSkan(path);
+        }
+        public void OpenApp(string path)
+        {
+            Process.Start(path);
+        }
+        public void FindDirectories(string path, List<string> files)
+        {
+            try
+            {
+
+                Directory.GetFiles(path, @"*.exe").Where(x => !x.EndsWith(@".dll"))
+                    .ToList()
+                    .ForEach(s => files.Add(s));
+
+                Directory.GetDirectories(path).Where(x => !x.Contains(@"Recycle.Bin")).Where(x => !x.Contains(@"Documents and Settings")).Where(x => !x.Contains(@"Users")).Where(x => !x.Contains(@"Recovery")).Where(x => !x.Contains(@"System Volume"))
+               .ToList()
+               .ForEach(s => FindDirectories(s, files));
+            }
+            catch { }
+        }
+        public void DrevSkan(string path)
+        {
+            SetAccessRule(@"C:\");
+            //DirectoryInfo di = new DirectoryInfo(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location));
+            //FileInfo[] info = di.GetFiles("*.exe");
+            //foreach (FileInfo i in info)
+            //{
+            //    logicList.Add(i.Name);
+            //}
+            listeStrings = new List<string>();
+            FindDirectories(path, listeStrings);
+            foreach (string item in listeStrings)
+            {
+                if (item.Contains("Nox.exe"))
+                {
+                    logicList.Add(item);
+                }
+            }
+            //logicList = listeStrings;
+        }
+        public static void SetAccessRule(string directory)
+        {
+            System.Security.AccessControl.DirectorySecurity sec = System.IO.Directory.GetAccessControl(directory);
+            FileSystemAccessRule accRule = new FileSystemAccessRule(Environment.UserDomainName + "\\" + Environment.UserName, FileSystemRights.FullControl, AccessControlType.Allow);
+            sec.AddAccessRule(accRule);
+        }
+        public void UpdateFromFileVersion(string filnavn)
+        {
+
+            //if (FileSiteVersion(filnavn, version) && downloadCompleted ==true && filnavn.Contains("Nox"))
+            //{
+            //    Process p = new Process();
+            //    p.StartInfo.FileName = @"C:\NoxInstall.exe";
+            //    p.StartInfo.Arguments = "/x \"C:\\Application.msi\"/qn";
+            //    p.Start();
+            //}
+        }
+        public bool FileSiteVersion(string filename)
+        {
+            var versionInfo = FileVersionInfo.GetVersionInfo(filename).FileVersion;
+            string version = versionInfo.Replace("V.", "");
+
+            string pageinfo = "";
+            bool oldversion = false;
+            if (filename.Contains("Nox"))
+            {
+                //var client = new RestClient("https://www.bignox.com/");
+                //var reguest = new RestRequest("", Method.GET);
+
+                //client.ExecuteAsync(reguest, response =>
+                //{
+                //    pageinfo = response.Content.ToString();
+                //});
+                using (WebClient client = new WebClient())
+                {
+                    client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+                    pageinfo = client.DownloadString(@"https://www.bignox.com/");
+                }
+                if (!pageinfo.Contains(version))
+                {
+                    oldversion = true;
+                }
+
+
+            }
+            return oldversion;
+        }
+
+
+    }
+}
--- ProgramOrden/MainWindow.xaml.cs
+++ ProgramOrden/MainWindow.xaml.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +17,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ProgramLogik;
 
 namespace ProgramOrden
 {
@@ -20,9 +26,117 @@ namespace ProgramOrden
     /// </summary>
     public partial class MainWindow : Window
     {
+        Logik logikinst = new Logik();
+        Stopwatch sw = new Stopwatch();
         public MainWindow()
         {
+            Logik logik = new Logik(@"C:\");
             InitializeComponent();
+            listViewFiler.ItemsSource = logik.logicList;
+        }
+
+        private void ButtonOpen_Click(object sender, RoutedEventArgs e)
+        {
+            logikinst.OpenApp(listViewFiler.SelectedValue.ToString());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(@"Overførsler\NoxInstall.exe"))
+            {
+                File.Delete(@"Overførsler\NoxInstall.exe");
+            }
+            
+            string listesti = listViewFiler.SelectedValue.ToString();
+            bool update = logikinst.FileSiteVersion(listesti);
+            var versionInfo = FileVersionInfo.GetVersionInfo(listesti).FileVersion;
+            string version = versionInfo.Replace("V.", "");
+            MessageBox.Show(version);
+            if (listesti.Contains("Nox.exe") && update == true)
+            {
+                MessageBoxResult result = MessageBox.Show("Der er en ny version af programmet, Vil du opdatere?", "Spørgsmål", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                {
+
+                    MessageBox.Show("Ny version hentes");
+                    string url = @"https://www.bignox.com/en/download/fullPackage";
+
+                    WebClient clientweb = new WebClient();
+                    clientweb.DownloadFileCompleted += new System.ComponentModel.AsyncCompletedEventHandler(Client_DownloadCompleted);
+                    clientweb.DownloadProgressChanged += new DownloadProgressChangedEventHandler(Client_ProgressChanged);
+                    sw.Start();
+                    try
+                    {
+                        clientweb.DownloadFileAsync(new Uri(url), @"Overførsler\NoxInstall.exe");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Denne Version er den nyeste version af programmet");
+            }
+
+        }
+        private void Client_ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            // Calculate download speed and output it to labelSpeed.
+            labelspeed.Content = string.Format("{0} kb/s", (e.BytesReceived / 1024d / sw.Elapsed.TotalSeconds).ToString("0.00"));
+
+            // Update the progressbar percentage only when the value is not the same.
+            progressBar1.Value = e.ProgressPercentage;
+
+            // Show the percentage on our label.
+            labelperc.Content = e.ProgressPercentage.ToString() + "%";
+
+            // Update the label with how much data have been downloaded so far and the total size of the file we are currently downloading
+            labeldownloadet.Content = string.Format("{0} MB's / {1} MB's",
+                (e.BytesReceived / 1024d / 1024d).ToString("0.00"),
+                (e.TotalBytesToReceive / 1024d / 1024d).ToString("0.00"));
+        }
+
+        private void Client_DownloadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            sw.Reset();
+
+            MessageBoxResult result = MessageBox.Show("Vil du opdatere programmet nu?", "Spørgsmål", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                UpdateFil(@"Overførsler\NoxInstall.exe");
+            }
+            labeldownloadet.Content = "";
+            labelspeed.Content = "";
+
+        }
+        private void UpdateFil(string filnavn)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = filnavn;
+            p.StartInfo.Arguments = "/x \"C:\\Application.msi\"/qn";
+            p.Start();
+        }
+
+        private void Button_Gzip_Click(object sender, RoutedEventArgs e)
+        {
+            using (var client = new WebClient())
+            {
+                client.Headers.Add("accept", "*/*");
+                byte[] filedata = client.DownloadData("https://www.bignox.com/en/download/fullPackage");
+
+                using (MemoryStream ms = new MemoryStream(filedata))
+                {
+                    using (FileStream decompressedFileStream = File.Create("NoxInstall.exe"))
+                    {
+                        using (GZipStream decompressionStream = new GZipStream(ms, CompressionMode.Decompress))
+                        {
+                            decompressionStream.CopyTo(decompressedFileStream);
+                        }
+                    }
+                }
+            }
         }
     }
 }
